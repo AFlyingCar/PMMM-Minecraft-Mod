d08ee02ba4f2dc4e5c0fa74e98aab9f9a3f90705
Tyler Robbins <tyler@familyrobbins.com>
2016-01-02 12:11:10 -0800

    Added a WitchMinionFactory class and a Factory class for creating the other factory class in a factory-like way. Factorially.

---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/PMWitchMinionFactory.java
@@ -0,0 +1,14 @@
+package com.MadokaMagica.mod_madokaMagica.factories;
+
+import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitchMinion;
+
+public class PMWitchMinionFactory{
+    public static EntityPMWitchMinion generate(PMDataTracker pd){
+//         // create based on liked entity types
+//         // AI based on player pacivity
+//         // if th eplayer liked fighting, the minions will be more aggressive
+    	return null;
+    }
+}
+
---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/PMWitchMinionFactoryFactory.java
@@ -0,0 +1,11 @@
+package com.MadokaMagica.mod_madokaMagica.factories;
+
+import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitch;
+import com.MadokaMagica.mod_madokaMagica.factories.PMWitchMinionFactory;
+
+public class PMWitchMinionFactoryFactory{
+	public static PMWitchMinionFactory generate(PMDataTracker pd,EntityPMWitch witch){
+		return null;
+	}
+}
