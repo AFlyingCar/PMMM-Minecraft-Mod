31d3232fce4d02aa0638dc7aaced08d9aa9bd3d1
Tyler Robbins <tyler@familyrobbins.com>
2016-01-02 12:16:37 -0800

    Added the Witch entity classes. There is no code here yet, they are just here to satisfy the compiler until I can figure out how to program them properly .

---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitch.java
@@ -0,0 +1,12 @@
+package com.MadokaMagica.mod_madokaMagica.entities;
+
+import net.minecraft.entity.monster.EntityMob;
+
+import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+
+public class EntityPMWitch extends EntityMob{
+	public EntityPMWitch(PMDataTracker pd){
+		super(null);
+	}
+}
+
---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitchMinion.java
@@ -0,0 +1,10 @@
+package com.MadokaMagica.mod_madokaMagica.entities;
+
+import net.minecraft.entity.monster.EntityMob;
+
+public class EntityPMWitchMinion extends EntityMob{
+	public EntityPMWitchMinion(){
+		super(null);
+	}
+}
+
