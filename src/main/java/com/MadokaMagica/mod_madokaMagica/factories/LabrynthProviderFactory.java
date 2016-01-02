8a60537466e4f2e11e86c5964a95b1b49d623fba
Tyler Robbins <tyler@familyrobbins.com>
2016-01-02 12:14:59 -0800

    Added a factory class for producing LabrynthProvider objects.

---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/LabrynthProviderFactory.java
@@ -0,0 +1,12 @@
+package com.MadokaMagica.mod_madokaMagica.factories;
+
+import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+import com.MadokaMagica.mod_madokaMagica.world.LabrynthProvider;
+import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitch;
+
+public class LabrynthProviderFactory{
+	public static LabrynthProvider generate(PMDataTracker pd,EntityPMWitch witch){
+		return null;
+	}
+}
+
