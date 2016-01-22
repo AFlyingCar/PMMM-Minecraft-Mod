3ad9990e07b8848e862ee050fd7ec4998636b13c
Tyler Robbins <tyler@familyrobbins.com>
2016-01-21 17:34:27 -0800

    Added a static instance member, as well as a getInstance method.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/events/MadokaMagicaPuellaMagiTransformationEvent.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/events/MadokaMagicaPuellaMagiTransformationEvent.java
@@ -3,5 +3,12 @@ package com.MadokaMagica.mod_madokaMagica.events;
 import com.MadokaMagica.mod_madokaMagica.events.MadokaMagicaEvent;
 
 public class MadokaMagicaPuellaMagiTransformationEvent extends MadokaMagicaEvent{
+    private static MadokaMagicaPuellaMagiTransformationEvent instance;
 
+    public static MadokaMagicaPuellaMagiTransformationEvent getInstance(){
+        if(MadokaMagicaPuellaMagiTransformationEvent.instance == null) 
+            MadokaMagicaPuellaMagiTransformationEvent.instance = new MadokaMagicaPuellaMagiTransformationEvent();
+        return MadokaMagicaPuellaMagiTransformationEvent.instance;
+    }
 }
+
