5779b171964db0191c2fe9a616c677c27804be01
Tyler Robbins <tyler@familyrobbins.com>
2016-03-02 19:22:03 -0800

    Changed datatrackers to be a HashMap of usernames and Data trackers. Added a method to get a datatracker by username. Modified the rest of the class to reflect the new datatype of datatrackers.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/managers/PlayerDataTrackerManager.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/managers/PlayerDataTrackerManager.java
@@ -2,33 +2,46 @@ package com.MadokaMagica.mod_madokaMagica.managers;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import net.minecraft.entity.player.EntityPlayer;
 
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
 
 public class PlayerDataTrackerManager{
-	private List<PMDataTracker> datatrackers;
+	private HashMap<String,PMDataTracker> datatrackers;
 	private static PlayerDataTrackerManager instance;
 
 	private PlayerDataTrackerManager(){
-		datatrackers = new ArrayList<PMDataTracker>();
+		datatrackers = new HashMap<String,PMDataTracker>();
 	}
 
 	public void addDataTracker(PMDataTracker tracker){
-		datatrackers.add(tracker);
+        datatrackers.put(""+tracker.player.getDisplayName(),tracker);
 	}
 
 	public PMDataTracker getTrackerByPlayer(EntityPlayer p){
+        if(datatrackers.containsKey(p.getDisplayName()))
+            return datatrackers.get(""+p.getDisplayName());
+        return null;
+        /*
 		for(PMDataTracker tracker : datatrackers)
 			if(tracker.getPlayer() == p)
 				return tracker;
 		return null;
+        */
 	}
 
+    public PMDataTracker getTrackerByUsername(String name){
+        if(datatrackers.containsKey(name))
+            return datatrackers.get(name);
+        return null;
+    }
+
 	public void manage(){
-		for(PMDataTracker tracker : datatrackers)
-			tracker.updateData();
+		for(Entry<String,PMDataTracker> trackerset : datatrackers.entrySet())
+			trackerset.getValue().updateData();
 	}
 
 	public static PlayerDataTrackerManager getInstance(){
