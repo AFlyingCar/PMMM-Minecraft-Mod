eccbd84e4f67c9d9fc4c4a3db0bdf26d243ca864
Tyler Robbins <tyler@familyrobbins.com>
2016-03-08 10:39:24 -0800

    Finished Wish class.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/util/Wish.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/util/Wish.java
@@ -1,7 +1,29 @@
 package com.MadokaMagica.mod_madokaMagica.util;
 
+import net.minecraft.command.ICommandSender;
+
 public class Wish{
-	public Wish(String start, String command, String parameters){
-		
+    private String command;
+    private String message;
+    private ICommandSender sender;
+	public Wish(String command, ICommandSender sender,String message){
+        this.command = command;
+        this.message = message;
+        this.sender = sender;
 	}
+    public Wish(){
+    }
+
+    public String getCommand(){
+        return command;
+    }
+    public String getMessage(){
+        return message;
+    }
+    public ICommandSender getSender(){
+        return sender;
+    }
+    public boolean isQuestion(){
+        return message.endsWith("?");
+    }
 }
