c4035dac339f570bdf85c81442c643af935362db
Tyler Robbins <tyler@familyrobbins.com>
2016-03-10 23:23:33 -0800

    Did some stuff.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/managers/IncubatorManager.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/managers/IncubatorManager.java
@@ -1,13 +1,16 @@
 package com.MadokaMagica.mod_madokaMagica.managers;
 
+import java.util.ArrayList;
+
 import net.minecraft.entity.player.EntityPlayer;
 
 import com.MadokaMagica.mod_madokaMagica.entities.EntityIncubator;
 
 public class IncubatorManager{
 	private static IncubatorManager instance;
+	private static ArrayList<EntityIncubator> incubator_list;
 	private IncubatorManager(){
-
+		incubator_list = new ArrayList<EntityIncubator>();
 	}
 
 	public static IncubatorManager getInstance(){
