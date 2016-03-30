ea366cb286b3e707c23214f104c688863a73cb52
Tyler Robbins <tyler@familyrobbins.com>
2016-03-30 12:29:54 -0700

    Added a member variable for a Wish object.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/events/MadokaMagicaPuellaMagiTransformationEvent.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/events/MadokaMagicaPuellaMagiTransformationEvent.java
@@ -3,13 +3,16 @@ package com.MadokaMagica.mod_madokaMagica.events;
 import cpw.mods.fml.common.eventhandler.Event;
 import net.minecraft.entity.player.EntityPlayer;
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+import com.MadokaMagica.mod_madokaMagica.util.Wish;
 
 public class MadokaMagicaPuellaMagiTransformationEvent extends Event{
     public PMDataTracker tracker;
+    public Wish wish;
 
-    public MadokaMagicaPuellaMagiTransformationEvent(PMDataTracker tracker){
+    public MadokaMagicaPuellaMagiTransformationEvent(PMDataTracker tracker,Wish wish){
         super();
         this.tracker = tracker;
+        this.wish = wish;
     }
 
     public MadokaMagicaPuellaMagiTransformationEvent(){
