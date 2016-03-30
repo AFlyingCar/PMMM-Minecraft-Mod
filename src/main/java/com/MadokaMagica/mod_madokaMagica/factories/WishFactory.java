534bc5292da4d521f8f2be1d9a3a3ea4c191b390
Tyler Robbins <tyler@familyrobbins.com>
2016-03-30 12:00:24 -0700

    Reimplemented the parameters pattern. Removed 'i' from buildUsernamePattern(). Added a new generateParts method which uses HashMaps instead of an ArrayList. Updated the matching patterns. Stopped using generateParts(...) and started using generatePartsHM(...) instead. Passed 'parts' to the Wish constructor in the generateWish(...) method.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/factories/WishFactory.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/WishFactory.java
@@ -1,6 +1,7 @@
 package com.MadokaMagica.mod_madokaMagica.factories;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.*;
 
 import net.minecraft.entity.player.EntityPlayer;
@@ -10,9 +11,9 @@ import com.MadokaMagica.mod_madokaMagica.util.Wish;
 import com.MadokaMagica.mod_madokaMagica.util.Helper;
 
 public class WishFactory{
-	public final static String startPattern = "^(kyuubey|hey kyuubey|incubator|coobie|excuse me kyuubey).";
-	public final static String commandPattern = ".+(can (%1$s) have ([0-9]+)|give ([0-9]+) to (%1$s)|give (%1$s) ([0-9]+)|teleport (%1$s) to (%1$s)|(%1$s) has a question).";
-	public final static String parameterPattern = "*()*";
+	public final static String startPattern = "^(kyuubey|hey kyuubey|incubator|coobie|excuse me kyuubey)+( |\t|,|\\.|\\?)*";
+	public final static String commandPattern = "(give)( |\t)+";
+	public final static String parameterPattern = "(1%s|[0-9]+ of id [0-9]+|[0-9]+)( |\t|,|\\.|\\?)*";
 
 	public static Wish generateWish(EntityPlayer player, String message){
 		return generateWish(player,message,false);
@@ -21,10 +22,11 @@ public class WishFactory{
 	public static Wish generateWish(EntityPlayer player, String message, boolean test){
 		Wish newWish = null;
 
-		ArrayList<String> parts = generateParts(player, message.toLowerCase(),test);
+        // ArrayList<String> parts = generateParts(player, message.toLowerCase(),test);
+		HashMap<String,ArrayList<String>> parts = generatePartsHM(player, message.toLowerCase(),test);
 		if(parts == null) return newWish;
 
-        newWish = new Wish(parts.get(1),player,message);
+        newWish = new Wish("",player,message,parts);
 
 		return newWish;
 	}
@@ -33,24 +35,73 @@ public class WishFactory{
 		return generateParts(player,message,false);
 	}
 
+    public static HashMap<String,ArrayList<String>> generatePartsHM(EntityPlayer player,String message, boolean test){
+        HashMap<String,ArrayList<String>> parts = new HashMap<String,ArrayList<String>>();
+
+        parts.put("START",new ArrayList<String>());
+        parts.put("COMM",new ArrayList<String>());
+        parts.put("PARAM",new ArrayList<String>());
+
+        Pattern start;
+        Pattern command;
+        Pattern params;
+        Pattern targets;
+
+        Matcher s_matches;
+        Matcher c_matches;
+        Matcher p_matches;
+        Matcher t_matches;
+
+        start = Pattern.compile(startPattern);
+        s_matches = start.matcher(message);
+
+        // Return null if the player wasn't addressing the incubator
+        if(!s_matches.matches() && !test) return null;
+
+        while(s_matches.find()){
+            parts.get("START").add(s_matches.group(1));
+        }
+
+        command = Pattern.compile(commandPattern);
+        c_matches = command.matcher(message);
+
+        if(!c_matches.matches() && !test) return null;
+
+        while(c_matches.find()){
+            parts.get("COMM").add(c_matches.group(1));
+        }
+
+        params = Pattern.compile(String.format(parameterPattern,buildUsernamePattern()));
+        p_matches = params.matcher(message);
+
+        // Don't return null here, because some commands may not have parameters
+        // if(p_matches.matches()){
+            while(p_matches.find()){
+                parts.get("PARAM").add(p_matches.group(1));
+            }
+        // }
+
+        return parts;
+    }
+
 	public static ArrayList<String> generateParts(EntityPlayer player, String message, boolean test){
 		ArrayList<String> parts = new ArrayList<String>();
 		Pattern start = Pattern.compile(startPattern);
 		Pattern command = Pattern.compile(String.format(commandPattern,buildUsernamePattern()));
-		//Pattern parameters = Pattern.compile(parameterPattern);
+		Pattern parameters = Pattern.compile(parameterPattern);
         Pattern targets = Pattern.compile("("+buildUsernamePattern()+")");
 
         // We check how it starts to see if message is even directed at the Incubator
 		Matcher start_matches = start.matcher(message);
 		Matcher command_matches = command.matcher(message);
-		//Matcher parameters_matches = parameters.matcher(message);
+		Matcher parameters_matches = parameters.matcher(message);
         Matcher targets_matches = targets.matcher(message);
 
 		// If any of them failed to match a string, do nothing.
         if(!(
                 start_matches.matches()
                 &&command_matches.matches()
-                //&&parameters_matches.matches()
+                &&parameters_matches.matches()
                 &&targets_matches.matches()
             ))
         {
@@ -77,7 +128,7 @@ public class WishFactory{
         for(String name : MinecraftServer.getServer().getAllUsernames()){
             pattern += name + "|";
         }
-        pattern += "me|i";
+        pattern += "me";
         return pattern;
     }
 }
