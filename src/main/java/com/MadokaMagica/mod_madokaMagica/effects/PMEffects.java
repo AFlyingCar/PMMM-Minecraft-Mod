1afffc0fb97255203a1843159e99bb2fbd934a6b
Tyler Robbins <tyler@familyrobbins.com>
2016-04-03 19:09:29 -0700

    Finished renderGradient method. Rewrote generateAggressiveOverlay(). Changed the call to draw(...). But none of that really matters, because I'm rewriting this whole class. It will use shaders, exactly the same as the built-in 'Super Secret Settings' feature. What we have now is: Added a HashMap that maps personality types (as Strings) to ResourceLocations that point to json files. Added a failure counter that checks how many times the effect failed to load/render. Rewrote applyEffects(...) to use this new system. The code itself is almost copy-pasted from EntityRenderer.activateNextShader().

--- src/main/java/com/MadokaMagica/mod_madokaMagica/effects/PMEffects.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/effects/PMEffects.java
@@ -1,13 +1,31 @@
 package com.MadokaMagica.mod_madokaMagica.effects;
 
-import java.nio.IntBuffer;
+/*
+ ******************************************************
+ *    READ ME! A LOT OF TEXT SO YOU DON'T MISS THIS   *
+ ******************************************************
+ *    NOTE: I Have figured out how to do this properly, or at least, a possible solution
+ *    Look at the Minecraft Source code, specifically, the files net.minecraft.client.EntityRenderer.java, net.minecraft.client.shader.ShaderGroup.java, net.minecraft.client.shader.Shader.java, net.minecraft.client.shader.Framebuffer.java, and net.minecraft.client.renderer.OpenGlHelper.java
+ *    These are the files that are involved in the Super Secret Settings part of Minecraft's options, which has a series of filters that almost seem to do exactly what I want.
+ *    What needs to happen now is I need to find out how those filters are created, and recreate them here, with some modifications of course. This will be much much easier than trying to reinvent the wheel like I've been doing.
+ *    Finally, this file is almost fucking done! :D
+ */
 
+import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.io.IOException;
+
+import com.google.gson.JsonSyntaxException;
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.shader.ShaderGroup;
 import net.minecraft.client.Minecraft;
 
 import com.MadokaMagica.mod_madokaMagica.util.Helper;
+import com.MadokaMagica.mod_madokaMagica.util.PMGuiHelper;
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
 
 public class PMEffects{
@@ -15,18 +33,58 @@ public class PMEffects{
     private static int frameCount = 10; // Should equal 10 at the start, so that lastOverlay is created the very first time generateMotionBlur is called
     private static IntBuffer lastOverlay = null;
 
+    public static int failureCount = 0;
+
+    public final static int MAXIMUM_FAILURE_COUNT = 20;
+
     private static int timer = 0;
 
     // A TEST COUNTER. DELETE ME SENPAI!
     private static int tick_counter = 0;
 
+    // Construct a map of personality types with ResourceLocations to where they live
+    private static HashMap<String,ResourceLocation> shaderLocations = new HashMap<String,ResourceLocation>(){
+        {
+            put("ARCHITECT",new ResourceLocation("shaders/post/architect.json"));
+            put("ENGINEERING",new ResourceLocation("shaders/post/engineering.json"));
+            put("GREED",new ResourceLocation("shaders/post/greed.json"));
+            put("WATER",new ResourceLocation("shaders/post/water.json"));
+            put("NATURE",new ResourceLocation("shaders/post/nature.json"));
+            put("DAY",new ResourceLocation("shaders/post/day.json"));
+            put("NIGHT",new ResourceLocation("shaders/post/night.json"));
+            put("VILLAIN",new ResourceLocation("shaders/post/villain.json"));
+            put("HERO",new ResourceLocation("shaders/post/hero.json"));
+            put("PASSIVE",new ResourceLocation("shaders/post/passive.json"));
+            put("AGGRESSIVE",new ResourceLocation("shaders/post/aggressive.json"));
+            put("DEFAULT",new ResourceLocation("shaders/post/default.json"));
+        }
+    };
+
     public static void applyPlayerEffects(PMDataTracker pmdt){
-        String personality = pmdt.getHighestScoreIden();
-        generateMotionBlur((int)((pmdt.getCorruption()/100)*PMEffects.MBLUR_MAX_FRAME_WAIT));
+        EntityRenderer renderer = Minecraft.getMinecraft().entityRenderer;
+
+        // If shaders aren't active, then we aren't doing anything anyways.
+        if(!renderer.isShaderActive()) failureCount = MAXIMUM_FAILURE_COUNT;
+
+        // Don't do anything if the failure rate is too high.
+        if(failureCount >= MAXIMUM_FAILURE_COUNT) return;
+        
         float opacity1 = calculateOpacity(pmdt,50,2);
         float opacity2 = calculateOpacity(pmdt,75,4);
-        renderOverlay(personality,opacity2);
-        renderGradient(opacity1);
+        
+        String personality = pmdt.getHighestScoreIden();
+
+        try{
+            // TODO: Find some way to tie opacity into each of these shaders
+            renderer.theShaderGroup = new ShaderGroup(renderer.mc.getTextureManager(),renderer.resourceManager,renderer.mc.getFrameBuffer(),shaderLocations.get(personality));
+            renderer.theShaderGroup.createBindFrameBuffers(renderer.mc.displayWidth,renderer.mc.displayHeight);
+        }catch(IOException exception){
+            failureCount++;
+            logger.warn("Unable to load shader \""+personality+"\". Failures="+failureCount,exception);
+        }catch (JsonSyntaxException jsonsyntaxexception){
+            failureCount++;
+            logger.warn("Unable to load shader \""+personality+"\". Failures="+failureCount,jsonsyntaxexception);
+        }
     }
 
     // A simple calculation to simulate 0%-100% within a range of lowestPercent%-100%
@@ -57,7 +115,8 @@ public class PMEffects{
             else if(personality.equals("AGGRESSIVE"))
                 overlay = generateAggressiveOverlay();
             else
-                overlay = generateMotionBlurOverlay();
+                // overlay = generateMotionBlurOverlay();
+                overlay = generateAggressiveOverlay(); // Replaced motion blur for debugging purposes so I don't have to run the long ass command every fucking time I want to test this
             overlay = applyTransparencyReal(overlay, opacity);
 
             lastOverlay = overlay;
@@ -75,7 +134,7 @@ public class PMEffects{
 
         if(overlay != null){
             System.out.println("Calling glDrawPixels(int,int,int,int,IntBuffer) at tick="+tick_counter);
-            draw(width,height,GL11.GL_COLOR_INDEX,GL11.GL_UNSIGNED_BYTE,overlay);
+            draw(width,height,GL11.GL_RED,GL11.GL_UNSIGNED_BYTE,overlay);
         }
     }
 
@@ -88,10 +147,35 @@ public class PMEffects{
             @ Post render
             1282: Invalid Operation
         */
+        // Funny thing I just noticed, this is actually occurring because of glReadPixels over in .../util/Helper.java
+        // Of course, I didn't notice since it's not even printing anything remotely useful
+        // THANKS OPENGL
     }
 
-    private static void renderGradient(float opacity){
-        int[] color = {0,0,0,255};
+    private static void renderGradient(String personality,float opacity){
+        int[] fcolor;
+        int[] tcolor = new int[]{0,0,0,0}; // Nothing
+        int a = (int)(255*opacity);
+        if(personality.equals("NATURE"))
+            fcolor = new int[]{0,255,0,a};
+        else if(personality.equals("WATER"))
+            fcolor = new int[]{0,0,255,a};
+        else if(personality.equals("GREED"))
+            fcolor = new int[]{255,215,0,a};
+        else if(personality.equals("NIGHT"))
+            fcolor = new int[]{0,0,0,a};
+        else if(personality.equals("AGGRESSIVE"))
+            fcolor = new int[]{255,0,0,a};
+        else
+            fcolor = new int[]{255,255,255,a};
+
+        ScaledResolution sres = new ScaledResolution(Minecraft.getMinecraft(),
+            Minecraft.getMinecraft().displayWidth,
+            Minecraft.getMinecraft().displayHeight);
+        int width = sres.getScaledWidth();
+        int height = sres.getScaledHeight();
+
+        PMGuiHelper.drawGradientRectGetAround(0,0,width,height,Helper.RGBToHex(fcolor),Helper.RGBToHex(tcolor));
     }
 
     private static IntBuffer generateHeroOverlay(){
@@ -105,12 +189,14 @@ public class PMEffects{
     }
 
     private static IntBuffer generateAggressiveOverlay(){
-        IntBuffer screen = Helper.getScreenPixels();
-        int[] colors = new int[screen.array().length];
-        for(int i=0;i<colors.length;i++){
-            colors[i] = screen.get(i)&0xFF0000;
-        }
-        return IntBuffer.wrap(colors);
+        return Helper.getScreenPixels(GL11.GL_RED,GL11.GL_UNSIGNED_BYTE,4);
+
+        // IntBuffer screen = Helper.getScreenPixels();
+        // int[] colors = new int[screen.array().length];
+        // for(int i=0;i<colors.length;i++){
+        //     colors[i] = screen.get(i)&0xFF0000;
+        // }
+        // return IntBuffer.wrap(colors);
     }
 
     private static IntBuffer generateNatureOverlay(){
