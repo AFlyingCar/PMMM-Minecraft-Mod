08bc982a7d108585066d4321ebf2d228b4b356ea
Tyler Robbins <tyler@familyrobbins.com>
2016-05-25 20:00:12 -0700

    Fixed the super constructor call.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitch.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitch.java
@@ -6,7 +6,7 @@ import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
 
 public class EntityPMWitch extends EntityMob{
 	public EntityPMWitch(PMDataTracker pd){
-		super(null);
+		super(pd.player.worldObj);
 	}
 }
 
