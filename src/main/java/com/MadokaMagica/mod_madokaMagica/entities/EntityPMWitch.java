db589cf3650f950a655ee992a4ed27db3499053f
Tyler Robbins <tyler@familyrobbins.com>
2016-05-28 08:54:24 -0700

    Added member variables for PMDataTracker and EntityPMWitchLabrynthEntrance, as well as imports for both.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitch.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitch.java
@@ -3,8 +3,11 @@ package com.MadokaMagica.mod_madokaMagica.entities;
 import net.minecraft.entity.monster.EntityMob;
 
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitchLabrynthEntrance;
 
 public class EntityPMWitch extends EntityMob{
+	public PMDataTracker tracker;
+	public EntityPMWitchLabrynthEntrance entrance;
 	public EntityPMWitch(PMDataTracker pd){
 		super(pd.player.worldObj);
 	}
