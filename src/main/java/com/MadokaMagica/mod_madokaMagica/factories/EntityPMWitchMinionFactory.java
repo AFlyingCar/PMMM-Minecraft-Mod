59026673ff3b64930902e156f6ea3659e6eab1d3
Tyler Robbins <tyler@familyrobbins.com>
2016-06-11 16:39:43 -0700

    Fixed tabbing issues.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/factories/EntityPMWitchMinionFactory.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/EntityPMWitchMinionFactory.java
@@ -14,54 +14,54 @@ import com.MadokaMagica.mod_madokaMagica.renderers.RenderPMWitchMinionHolder;
 import com.MadokaMagica.mod_madokaMagica.factories.RenderPMWitchMinionFactory;
 
 public class EntityPMWitchMinionFactory {
-	public int aggressiveLevel;
-	public EntityPMWitch witch;
-	public EntityPMWitchLabrynthEntrance home;
-	public PMDataTracker tracker;
+    public int aggressiveLevel;
+    public EntityPMWitch witch;
+    public EntityPMWitchLabrynthEntrance home;
+    public PMDataTracker tracker;
 
-	public EntityPMWitchMinion createMinion(World worldObj){
-		EntityPMWitchMinion minion = new EntityPMWitchMinion(worldObj,witch,home,tracker);
-		// choose AI
-		switch(aggressiveLevel){
-			case 0:
-				this.setupZombieLikeAITasks(minion);
-				break;
-			case 1:
-				// guard labrynth entrance
-				this.setupGuardEntranceAITasks(minion);
-				break;
-			case 2:
-				// guard witch
-				this.setupGuardWitchAITasks(minion);
-				break;
-		}
+    public EntityPMWitchMinion createMinion(World worldObj){
+        EntityPMWitchMinion minion = new EntityPMWitchMinion(worldObj,witch,home,tracker);
+        // choose AI
+        switch(aggressiveLevel){
+            case 0:
+                this.setupZombieLikeAITasks(minion);
+                break;
+            case 1:
+                // guard labrynth entrance
+                this.setupGuardEntranceAITasks(minion);
+                break;
+            case 2:
+                // guard witch
+                this.setupGuardWitchAITasks(minion);
+                break;
+        }
 
-		RenderPMWitchMinion renderer = RenderPMWitchMinionFactory.createRenderer(minion);
-		RenderPMWitchMinionHolder.getInstance().addEntity(minion,renderer);
+        RenderPMWitchMinion renderer = RenderPMWitchMinionFactory.createRenderer(minion);
+        RenderPMWitchMinionHolder.getInstance().addEntity(minion,renderer);
 
-		// TODO: do something to create the model
+        // TODO: do something to create the model
 
-		return minion;
-	}
+        return minion;
+    }
 
-	public void setupZombieLikeAITasks(EntityPMWitchMinion minion){
-		clearAITasks(minion);
-		minion.tasks.addTask(0,new EntityAIEscortVillager(minion));
-		minion.tasks.addTask(1,new EntityAIMoveTowardsTarget(minion,0.05,100)); // speed and maximum distance (in blocks)
-		minion.targetTasks.addTask(2,new EntityAIBewitchVillager(minion));
-	}
+    public void setupZombieLikeAITasks(EntityPMWitchMinion minion){
+        clearAITasks(minion);
+        minion.tasks.addTask(0,new EntityAIEscortVillager(minion));
+        minion.tasks.addTask(1,new EntityAIMoveTowardsTarget(minion,0.05,100)); // speed and maximum distance (in blocks)
+        minion.targetTasks.addTask(2,new EntityAIBewitchVillager(minion));
+    }
 
-	public void setupGuardEntranceAITasks(EntityPMWitchMinion minion){
-		// TODO: Finish this method
-	}
+    public void setupGuardEntranceAITasks(EntityPMWitchMinion minion){
+        // TODO: Finish this method
+    }
 
-	public void setupGuardWitchAITasks(EntityPMWitchMinion minion){
-		// TODO: Finish this method
-	}
+    public void setupGuardWitchAITasks(EntityPMWitchMinion minion){
+        // TODO: Finish this method
+    }
 
-	public void clearAITasks(EntityPMWitchMinion entity){
-		entity.tasks.taskEntries.clear();
-		entity.targetTasks.taskEntries.clear();
-	}
+    public void clearAITasks(EntityPMWitchMinion entity){
+        entity.tasks.taskEntries.clear();
+        entity.targetTasks.taskEntries.clear();
+    }
 }
 
