3079f534d3a4f0d64a57bcb440f0cca931582b0d
Tyler Robbins <tyler@familyrobbins.com>
2016-06-11 16:41:03 -0700

    Completed the createRenderer method. Fixed tabbing.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/factories/RenderPMWitchMinionFactory.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/RenderPMWitchMinionFactory.java
@@ -1,11 +1,20 @@
 package com.MadokaMagica.mod_madokaMagica.factories;
 
+import net.minecraft.client.model.ModelBase;
+
 import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitchMinion;
 import com.MadokaMagica.mod_madokaMagica.renderers.RenderPMWitchMinion;
+import com.MadokaMagica.mod_madokaMagica.factories.ModelPMWitchMinionFactory;
+import com.MadokaMagica.mod_madokaMagica.models.ModelPMWitchMinion;
 
 public class RenderPMWitchMinionFactory {
-	public static RenderPMWitchMinion createRenderer(EntityPMWitchMinion entity){
-		// TODO: Finish this method
-		return null;
-	}
+    public static RenderPMWitchMinion createRenderer(EntityPMWitchMinion entity){
+        ModelBase model = ModelPMWitchMinionFactory.createModel(entity);
+        float shadowSize = 1.0F; // TODO: Figure out how we can create dynamic shadow sizes
+
+        RenderPMWitchMinion renderer = new RenderPMWitchMinion(model,shadowSize);
+        renderer.minionTexture = ModelPMWitchMinionFactory.createTexture(entity,(ModelPMWitchMinion)model);
+
+        return renderer;
+    }
 }
