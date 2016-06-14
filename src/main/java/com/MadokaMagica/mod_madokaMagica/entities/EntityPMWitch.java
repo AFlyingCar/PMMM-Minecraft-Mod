f0081fbe04da138edcacb2f2ab50344f36c24d4c
Tyler Robbins <tyler@familyrobbins.com>
2016-06-14 13:24:46 -0700

    Added imports. Added some methods to fix a compile error. Changed reference of PMDataTracker.player to PMDataTracker.entity.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitch.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityPMWitch.java
@@ -1,6 +1,8 @@
 package com.MadokaMagica.mod_madokaMagica.entities;
 
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
 
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
 import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitchLabrynthEntrance;
@@ -9,7 +11,17 @@ public class EntityPMWitch extends EntityMob{
 	public PMDataTracker tracker;
 	public EntityPMWitchLabrynthEntrance entrance;
 	public EntityPMWitch(PMDataTracker pd){
-		super(pd.player.worldObj);
+		super(pd.entity.worldObj);
+	}
+
+	public EntityPMWitch(World world){
+		super(world);
+	}
+
+	@Override
+	public void readEntityFromNBT(NBTTagCompound base){
+		// TODO: Load a tracker based on this entity's getPersistentID()
+		super.readEntityFromNBT(base);
 	}
 }
 
