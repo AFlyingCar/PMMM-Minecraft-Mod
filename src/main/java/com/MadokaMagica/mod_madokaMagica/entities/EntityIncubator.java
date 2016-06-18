e238389896063e64857c9a52446cd5c12ab5bc94
Tyler Robbins <tyler@familyrobbins.com>
2016-06-18 14:24:38 -0700

    Added some debug print statements. Fixed tabbing.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityIncubator.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/EntityIncubator.java
@@ -11,36 +11,40 @@ import com.MadokaMagica.mod_madokaMagica.util.Wish;
 import com.MadokaMagica.mod_madokaMagica.factories.WishFactory;
 
 public class EntityIncubator extends EntityCreature{
-	private Map<EntityPlayer,Wish> wishByPlayer;
+    private Map<EntityPlayer,Wish> wishByPlayer;
 
-	public EntityIncubator(World world){
-		super(world);
-	}
+    public EntityIncubator(World world){
+        super(world);
+        System.out.println("EntityIncubator(World) - Constructor");
+    }
 
-	public boolean processChat(EntityPlayer player, String message){
-		Wish wish = WishFactory.generateWish(player,message);
-		if(wish != null){
-			wishByPlayer.put(player,wish);
-			return true;
-		}
-		return false;
-	}
+    public boolean processChat(EntityPlayer player, String message){
+        Wish wish = WishFactory.generateWish(player,message);
+        if(wish != null){
+            wishByPlayer.put(player,wish);
+            return true;
+        }
+        return false;
+    }
 
-	// TODO: Finish this method
-	@Override
-	public void entityInit(){
-		
-	}
+    // TODO: Finish this method
+    @Override
+    public void entityInit(){
+        System.out.println("EntityIncubator.entityInit()");
+        super.entityInit();
+    }
 
-	// TODO: Finish this method
-	@Override
-	public void writeEntityToNBT(NBTTagCompound tag){
-		
-	}
+    // TODO: Finish this method
+    @Override
+    public void writeEntityToNBT(NBTTagCompound tag){
+        System.out.println("EntityIncubator.writeEntityToNBT(NBTTagCompound)");
+        super.writeEntityToNBT(tag);
+    }
 
-	// TODO: Finish this method
-	@Override
-	public void readEntityFromNBT(NBTTagCompound tag){
-		
-	}
+    // TODO: Finish this method
+    @Override
+    public void readEntityFromNBT(NBTTagCompound tag){
+        System.out.println("EntityIncubator.readEntityFromNBT(NBTTagCompound)");
+        super.readEntityFromNBT(tag);
+    }
 }
