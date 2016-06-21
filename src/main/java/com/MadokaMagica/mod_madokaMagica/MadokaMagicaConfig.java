531f7226c094ac0ec1a4e7ba0d648928f9ac9b1f
Tyler Robbins <tyler@familyrobbins.com>
2016-06-21 13:22:37 -0700

    Added a class which handles the loading of configuration data.

---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/MadokaMagicaConfig.java
@@ -0,0 +1,25 @@
+package com.MadokaMagica.mod_madokaMagica;
+
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+
+public class MadokaMagicaConfig{
+    public static boolean useDebugModels;
+
+    public static void loadConfig(FMLPreInitializationEvent event){
+        Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+
+        try{
+            cfg.load();
+
+            useDebugModels = cfg.get("Debug","UseDebugModels",false).getBoolean(false);
+
+        }catch(Exception e){
+            FMLLog.severe("OH GOD WHY?!\n");
+        }finally{
+            cfg.save();
+        }
+    }
+}
+
