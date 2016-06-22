af399ee67117fd349a82a51a34fc47d2077ad680
Tyler Robbins <tyler@familyrobbins.com>
2016-06-21 20:28:42 -0700

    Rewrote a large part of ItemSoulGemManager so that it maps ItemStack objects of ItemSoulGem to their respective EntityPlayers (owners). Commented out a bunch of code (as I'm not sure how much each method is depended on by other classes yet).

--- src/main/java/com/MadokaMagica/mod_madokaMagica/managers/ItemSoulGemManager.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/managers/ItemSoulGemManager.java
@@ -1,35 +1,63 @@
 package com.MadokaMagica.mod_madokaMagica.managers;
 
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 
 import com.MadokaMagica.mod_madokaMagica.items.ItemSoulGem;
+import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
 
 public class ItemSoulGemManager{
-    private ArrayList<ItemSoulGem> all_soul_gems;
+    private Map<ItemStack,PMDataTracker> soulgem_player_map;
     private static ItemSoulGemManager instance;
 
     private ItemSoulGemManager(){
-        all_soul_gems = new ArrayList<ItemSoulGem>();
+        soulgem_player_map = new HashMap<ItemStack,PMDataTracker>();
     }
 
-    // private ItemSoulGemManager(ArrayList<ItemSoulGem> list){
-    //     all_soul_gems = list;
-    // }
-
-    public void addSoulGem(ItemSoulGem sg){
-        all_soul_gems.add(sg);
+    public void registerSoulGem(ItemStack stack,PMDataTracker player){
+        if(stack.getItem() instanceof ItemSoulGem)
+            soulgem_player_map.put(stack,player);
+        else
+            System.out.println("Unable to register soul gem! " + stack + " does not contain an ItemSoulGem!");
     }
 
+    public Map.Entry<ItemStack,PMDataTracker> deregisterSoulGem(ItemStack stack){
+        Set<Map.Entry<ItemStack,PMDataTracker>> set = soulgem_player_map.entrySet();
+        int index = -1;
+        for(int i=0;i<set.size();i++){
+            if(((Map.Entry<ItemStack,PMDataTracker>)set.toArray()[i]).getKey() == stack){
+                index = i;
+                break;
+            }
+        }
+        Map.Entry<ItemStack,PMDataTracker> entry;
+        if(index != -1){
+            entry = (Map.Entry<ItemStack,PMDataTracker>)(set.toArray()[index]);
+        }else{
+            return null;
+        }
+        soulgem_player_map.remove(stack);
+        return entry;
+    }
+
+    /*
     public void addAllSoulGems(ArrayList<ItemSoulGem> list){
         all_soul_gems.addAll(list);
     }
+    */
 
-    public ItemSoulGem getSoulGem(int index){
-        return all_soul_gems.get(index);
+    public PMDataTracker getPlayerForSoulGemStack(ItemStack stack){
+        if(stack.getItem() instanceof ItemSoulGem)
+            return soulgem_player_map.get(stack);
+        else
+            return null;
     }
     
+    /*
     public ItemSoulGem getSoulGemByPlayer(EntityPlayer player){
         for(ItemSoulGem sg : all_soul_gems)
             if(sg.player == player)
@@ -40,11 +68,14 @@ public class ItemSoulGemManager{
     public int getSoulGemCount(){
         return all_soul_gems.size();
     }
+    */
 
     public void manage(){
+        /*
         for(ItemSoulGem sg : all_soul_gems){
-            sg.update();
+            //sg.update();
         }
+        */
     }
 
     public static ItemSoulGemManager getInstance(){
