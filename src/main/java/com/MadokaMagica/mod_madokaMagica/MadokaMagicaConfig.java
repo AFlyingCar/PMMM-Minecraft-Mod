f8ae751c1888adc532e22e5ce44c1f91261a6188
Tyler Robbins <tyler@familyrobbins.com>
2016-06-22 12:38:07 -0700

    Added some more configuration options.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/MadokaMagicaConfig.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/MadokaMagicaConfig.java
@@ -7,6 +7,9 @@ import net.minecraftforge.common.config.Configuration;
 public class MadokaMagicaConfig{
     public static boolean useDebugModels;
     public static boolean enableCorruptionVisualEffects;
+    public static boolean killLabrynthEntranceWithoutWitchOrLabrynth;
+    public static int labrynthDimensionID;
+    public static int labrynthProviderID;
 
     public static void loadConfig(FMLPreInitializationEvent event){
         Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
@@ -14,9 +17,22 @@ public class MadokaMagicaConfig{
         try{
             cfg.load();
 
+            /*
+             * Debugging
+             */
             useDebugModels = cfg.get("Debug","UseDebugModels",false).getBoolean(false);
+            killLabrynthEntranceWithoutWitchOrLabrynth = cfg.get("Debug","KillLabrynthEntranceWithoutWitchOrLabrynth",false).getBoolean(false);
 
+            /*
+             * Effects
+             */
             enableCorruptionVisualEffects = cfg.get("Effects","EnableVisualEffects",false).getBoolean(false);
+
+            /*
+             * Dimensions
+             */
+            labrynthDimensionID = cfg.get("Dimension","LabrynthDimensionID",180).getInt(180);
+            labrynthProviderID = cfg.get("Dimension","LabrynthProviderID",180).getInt(180);
 
         }catch(Exception e){
             FMLLog.severe("OH GOD WHY?!\n");
