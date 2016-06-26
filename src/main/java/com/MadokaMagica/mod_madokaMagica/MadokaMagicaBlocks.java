77cdf485fb642f2d7c85824719e5060d3c04ada4
Tyler Robbins <tyler@familyrobbins.com>
2016-06-26 09:23:47 -0700

    Added some code which sets the CreativeTab of blocks to be our custom Creative Tab.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/MadokaMagicaBlocks.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/MadokaMagicaBlocks.java
@@ -3,12 +3,13 @@ package com.MadokaMagica.mod_madokaMagica;
 import net.minecraft.block.Block;
 
 import com.MadokaMagica.mod_madokaMagica.blocks.BlockLabrynthTeleporter;
+import com.MadokaMagica.mod_madokaMagica.MadokaMagicaMod;
 
 public class MadokaMagicaBlocks{
     public static Block labrynthTeleporter;
 
     public static void loadBlocks(){
-        labrynthTeleporter = new BlockLabrynthTeleporter().setBlockName("labrynthTeleporter");
+        labrynthTeleporter = new BlockLabrynthTeleporter().setBlockName("labrynthTeleporter").setCreativeTab(MadokaMagicaMod.PMMMCreativeTab);
     }
 }
 
