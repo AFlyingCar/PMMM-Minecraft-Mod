7f956b2ac60b697cffd6f44463e68f939cde7c15
Tyler Robbins <tyler@familyrobbins.com>
2016-06-29 18:35:55 -0700

    Added a factory which can produce LabrynthWorldServer objects.

---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/LabrynthWorldServerFactory.java
@@ -0,0 +1,27 @@
+package com.MadokaMagica.mod_madokaMagica.factories;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldSettings;
+import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
+
+import com.MadokaMagica.mod_madokaMagica.world.LabrynthWorldServer;
+import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+
+public class LabrynthWorldServerFactory{
+    public static LabrynthWorldServer createWorldServer(PMDataTracker tracker,int dimID){
+        World oworld = DimensionManager.getWorld(0);
+        LabrynthWorldServer worldServer = new LabrynthWorldServer(MinecraftServer.getServer(),
+                oworld.getSaveHandler(),
+                "Labrynth",
+                dimID,
+                new WorldSettings(oworld.getWorldInfo()),
+                MinecraftServer.getServer().theProfiler,
+                tracker
+        );
+        // TODO: Do something to set the internal variables of LabrynthWorldServer
+
+        return worldServer;
+    }
+}
+
