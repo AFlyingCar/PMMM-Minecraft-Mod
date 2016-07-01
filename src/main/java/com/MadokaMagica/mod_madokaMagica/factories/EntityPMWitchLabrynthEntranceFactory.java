1e3aa60eb1734e92962510019b6c38eb460ea0d2
Tyler Robbins <tyler@familyrobbins.com>
2016-07-01 12:27:38 -0700

    Added comment. Made createWitchLabrynthEntrance static. Changed how EntityPMWitchLabrynthEntrance is constructed (to match the new constructor). Removed TODO comments.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/factories/EntityPMWitchLabrynthEntranceFactory.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/EntityPMWitchLabrynthEntranceFactory.java
@@ -1,6 +1,7 @@
 package com.MadokaMagica.mod_madokaMagica.factories;
 
 import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
 import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitch;
@@ -8,11 +9,10 @@ import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitchLabrynthEntrance;
 import com.MadokaMagica.mod_madokaMagica.factories.LabrynthFactory.LabrynthDetails;
 
 public class EntityPMWitchLabrynthEntranceFactory {
-	public EntityPMWitchLabrynthEntrance createWitchLabrynthEntrance(LabrynthDetails details){
-		// TODO: Finish this method
-		EntityPMWitchLabrynthEntrance entrance = new EntityPMWitchLabrynthEntrance(details);
+	public static EntityPMWitchLabrynthEntrance createWitchLabrynthEntrance(LabrynthDetails details){
+        // Simply use the overworld from DimensionManager (Overworld is always Dimension 0) because all LabrynthEntrance's should exist only in the overworld
+		EntityPMWitchLabrynthEntrance entrance = new EntityPMWitchLabrynthEntrance(DimensionManager.getWorld(0),details);
 
-		// TODO: do something to create the model
 		return entrance;
 	}
 
