30b8c3c46b8bc833041f6d198c6f527f2dea131b
Tyler Robbins <tyler@familyrobbins.com>
2016-07-01 12:26:13 -0700

    Added calls to checkForNewVillage so that we can try to get a village immediately (rather than letting it sit at null). Added some code that checks if no villages exist and, if so, just choosing a random chunk instead. Added a DEBUG print statement.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/entities/ai/EntityAIWanderWithVillageBias.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/ai/EntityAIWanderWithVillageBias.java
@@ -1,5 +1,7 @@
 package com.MadokaMagica.mod_madokaMagica.entities.ai;
 
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.village.Village;
 
@@ -13,11 +15,13 @@ public class EntityAIWanderWithVillageBias extends EntityAIWanderWithChunkBias{
 
     public EntityAIWanderWithVillageBias(EntityCreature e,double s){
         super(e,null,s);
+        checkForNewVillage(); // Check for a new village immediately
         this.updateSpeed = 100;
     }
 
     public EntityAIWanderWithVillageBias(EntityCreature e, double s, int updateSpeed){
         super(e,null,s);
+        checkForNewVillage(); // Check for a new village immediately
         this.updateSpeed = updateSpeed;
     }
 
@@ -26,10 +30,20 @@ public class EntityAIWanderWithVillageBias extends EntityAIWanderWithChunkBias{
         if(this.currentTick%this.updateSpeed == 0)
             checkForNewVillage();
         this.currentTick++;
+        
         return super.shouldExecute();
     }
 
     private void checkForNewVillage(){
+        // If no villages actually exist, pick a random chunk as the travel destination
+        if(this.entity.worldObj.villageCollectionObj.getVillageList().size() == 0){
+            Chunk tempChunk = this.entity.worldObj.getChunkFromChunkCoords((int)this.entity.posX*2,(int)this.entity.chunkCoordZ*2);
+            this.chunk = new ChunkCoordinates(tempChunk.xPosition,0,tempChunk.zPosition);
+            return;
+        }else{
+            System.out.println("VillagList size: " + this.entity.worldObj.villageCollectionObj.getVillageList().size());
+        }
+
         Village vobj = this.entity.worldObj.villageCollectionObj.findNearestVillage(this.entity.chunkCoordX,
                 this.entity.chunkCoordY,
                 this.entity.chunkCoordZ,
