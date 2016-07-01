22c45f28acf131342ffe25804af09a415e61b15b
Tyler Robbins <tyler@familyrobbins.com>
2016-07-01 12:36:29 -0700

    Added imports. Removed useless provider variable (it was messing up calls to World.provider). Changed how tracker is initialized in the load method: It now gets created with no entity, and we simply load it later. Fixed an issue where we were calling the wrong methods on nbt in writeToNBT.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/world/LabrynthWorldServer.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/world/LabrynthWorldServer.java
@@ -1,19 +1,27 @@
 package com.MadokaMagica.mod_madokaMagica.world;
 
+import java.util.UUID;
+
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.WorldSettings;
 import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.entity.EntityLiving;
+import net.minecraftforge.common.DimensionManager;
+
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+import com.MadokaMagica.mod_madokaMagica.managers.PlayerDataTrackerManager;
+import com.MadokaMagica.mod_madokaMagica.util.Helper;
 
 public class LabrynthWorldServer extends WorldServer{
     public PMDataTracker tracker; // The tracker for the owner
     public boolean isCold;
     public int time; // 0 - Permanent day-time, 1 - Permanent night time, 2 - Permanent evening, 3 - Permanent morning
     public int stormy; // 0 - nothing, 1 - raining, 2 - thundering
-    public IChunkProvider provider;
+    //public IChunkProvider provider;
 
     public LabrynthWorldServer(MinecraftServer server, ISaveHandler saveHandler, String name, int dimID, WorldSettings settings, Profiler profiler,PMDataTracker tracker){
         super(server,saveHandler,name,dimID,settings,profiler);
@@ -22,7 +30,7 @@ public class LabrynthWorldServer extends WorldServer{
     }
 
     protected IChunkProvider createChunkProvider(){
-        return provider;
+        return chunkProvider;
     }
 
     public static boolean verifyNBTIntegrity(NBTTagCompound nbt){
@@ -36,9 +44,10 @@ public class LabrynthWorldServer extends WorldServer{
         if(!verifyNBTIntegrity(nbt))
             return null;
 
-        EntityLiving entity = Helper.getEntityLivingByUUID(new UUID(nbt.getLong("ENTITY_UUID_MOST_SIG"),nbt.getLong("ENTITY_UUID_LEAST_SIG")));
-        PMDataTracker tracker = new PMDataTracker(entity);
-        PlayerDataTrackerManager.getInstance().addTracker(tracker);
+        // The witch
+        //EntityLiving entity = Helper.getEntityLivingByUUID(new UUID(nbt.getLong("ENTITY_UUID_MOST_SIG"),nbt.getLong("ENTITY_UUID_LEAST_SIG")));
+        PMDataTracker tracker = new PMDataTracker();
+        PlayerDataTrackerManager.getInstance().addDataTracker(tracker);
 
         LabrynthWorldServer server = new LabrynthWorldServer(MinecraftServer.getServer(),
                 DimensionManager.getWorld(0).getSaveHandler(),
@@ -52,9 +61,9 @@ public class LabrynthWorldServer extends WorldServer{
     }
 
     public void writeToNBT(NBTTagCompound nbt){
-        nbt.setInteger("dimID",this.dimensionId);
-        nbt.setInteger("ENTITY_UUID_MOST_SIG",this.tracker.entity.getPersistentID().getMostSignificantBits());
-        nbt.setInteger("ENITTY_UUID_LEAST_SIG",this.tracker.entity.getPersistentID().getLeastSignificantBits());
+        nbt.setInteger("dimID",this.provider.dimensionId);
+        nbt.setLong("ENTITY_UUID_MOST_SIG",this.tracker.entity.getPersistentID().getMostSignificantBits());
+        nbt.setLong("ENITTY_UUID_LEAST_SIG",this.tracker.entity.getPersistentID().getLeastSignificantBits());
     }
 }
 
