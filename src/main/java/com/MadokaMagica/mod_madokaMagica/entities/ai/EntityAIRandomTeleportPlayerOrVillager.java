4f2771522ff44360339def1f2991f49336bff95e
Tyler Robbins <tyler@familyrobbins.com>
2016-07-01 12:23:35 -0700

    Made constants constant. Gave cooldown an initial value. Changed MutexBits value. Added DEBUG print statements.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/entities/ai/EntityAIRandomTeleportPlayerOrVillager.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/entities/ai/EntityAIRandomTeleportPlayerOrVillager.java
@@ -20,21 +20,23 @@ import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitchLabrynthEntrance;
 
 public class EntityAIRandomTeleportPlayerOrVillager extends EntityAIBase {
     // All entities within 15 blocks have the possibility of being teleported
-    public static double MAX_TELEPORT_DISTANCE = 15.0;
-    public static double MIN_TELEPORT_CHANCE = 0.05; // 5%
-    public static int COOLDOWN_WAIT_TIME = 30;
+    public static final double MAX_TELEPORT_DISTANCE = 15.0;
+    public static final double MIN_TELEPORT_CHANCE = 0.05; // 5%
+    public static final int COOLDOWN_WAIT_TIME = 30;
 
     private final EntityCreature entity;
     private AxisAlignedBB bbox;
-    private int cooldown;
+    private int cooldown = COOLDOWN_WAIT_TIME;
 
     public EntityAIRandomTeleportPlayerOrVillager(EntityCreature entity){
         this.entity = entity;
-        setMutexBits(1);
+        setMutexBits(2);
+        System.out.println("Constructing EntityAIRandomTeleportPlayerOrVillager");
     }
 
     @Override
     public boolean shouldExecute(){
+        System.out.println("shouldExecute(): cooldown = " + cooldown);
         cooldown++;
         if(cooldown >= COOLDOWN_WAIT_TIME){
             cooldown = 0;
@@ -45,6 +47,7 @@ public class EntityAIRandomTeleportPlayerOrVillager extends EntityAIBase {
 
     @Override
     public void startExecuting(){
+        System.out.println("Starting execution of AITask:EntityAIRandomTeleportPlayerOrVillager");
         bbox = AxisAlignedBB.getBoundingBox(entity.posX-MAX_TELEPORT_DISTANCE,
                 entity.posY-MAX_TELEPORT_DISTANCE,
                 entity.posZ-MAX_TELEPORT_DISTANCE,
