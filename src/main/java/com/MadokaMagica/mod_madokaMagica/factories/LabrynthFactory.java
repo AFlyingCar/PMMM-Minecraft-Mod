58f500de8aae27ba5337b401c871226604429593
Tyler Robbins <tyler@familyrobbins.com>
2016-07-04 19:49:13 -0700

    Added an extra flag to LabrynthDetails which specifies if the labrynth should be saved or not. This is so we don't keep destroyed labrynths. Moved the code which registers the new labrynth with DimensionManager to a method in LabrynthManager.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/factories/LabrynthFactory.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/LabrynthFactory.java
@@ -10,11 +10,13 @@ import com.MadokaMagica.mod_madokaMagica.factories.LabrynthWorldServerFactory;
 
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
 import com.MadokaMagica.mod_madokaMagica.entities.EntityPMWitch;
+import com.MadokaMagica.mod_madokaMagica.managers.LabrynthManager;
 
 public class LabrynthFactory{
     public static class LabrynthDetails{
         public int dimID;
         public String dimName;
+        public boolean markForDestruction;
         public LabrynthWorldServer world;
     }
 
@@ -26,16 +28,20 @@ public class LabrynthFactory{
 
         WorldServer customWorldServer = LabrynthWorldServerFactory.createWorldServer(tracker,dimID.intValue());
 
+        /*
         if(!DimensionManager.registerProviderType(dimID,LabrynthProvider.class,false))
             throw new IllegalStateException("There is a provider ID conflict between LabrynthProvider from MadokaMagica and another provider type. Somehow we were unable to get a new Provider ID.");
         DimensionManager.registerDimension(dimID,dimID); // The dimension and provider share an ID
         DimensionManager.initDimension(dimID);
         DimensionManager.setWorld(dimID,customWorldServer);
+        */
 
         LabrynthDetails details = new LabrynthDetails();
         details.dimID = dimID;
         details.dimName = dimName;
         details.world = (LabrynthWorldServer)customWorldServer;
+
+        LabrynthManager.getInstance().registerDetailsWithDimensionManager(details);
 
         return details;
     }
