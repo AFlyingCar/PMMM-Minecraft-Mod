16837379f1f4197020050c1b6a2312f97ce0f9cc
Tyler Robbins <tyler@familyrobbins.com>
2016-07-22 16:21:09 -0700

    Added a new block for LabrynthGeneration.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/MadokaMagicaBlocks.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/MadokaMagicaBlocks.java
@@ -1,15 +1,18 @@
 package com.MadokaMagica.mod_madokaMagica;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 
 import com.MadokaMagica.mod_madokaMagica.blocks.BlockLabrynthTeleporter;
 import com.MadokaMagica.mod_madokaMagica.MadokaMagicaMod;
 
 public class MadokaMagicaBlocks{
     public static Block labrynthTeleporter;
+    public static Block labrynthWallBlock;
 
     public static void loadBlocks(){
         labrynthTeleporter = new BlockLabrynthTeleporter().setBlockName("labrynthTeleporter").setCreativeTab(MadokaMagicaMod.PMMMCreativeTab);
+        labrynthWallBlock = Blocks.stone; // TODO: Make this an actual block, rather than a minecraft one
     }
 }
 
