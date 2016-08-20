02ed922b9bd199ce46016466e364a5494d7c8a81
Tyler Robbins <tyler@familyrobbins.com>
2016-08-19 17:08:34 -0700

    Modified factory to reflect changes in LabrynthWorldServer.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/factories/LabrynthWorldServerFactory.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/factories/LabrynthWorldServerFactory.java
@@ -7,9 +7,10 @@ import net.minecraftforge.common.DimensionManager;
 
 import com.MadokaMagica.mod_madokaMagica.world.LabrynthWorldServer;
 import com.MadokaMagica.mod_madokaMagica.trackers.PMDataTracker;
+import com.MadokaMagica.mod_madokaMagica.factories.LabrynthFactory.LabrynthDetails;
 
 public class LabrynthWorldServerFactory{
-    public static LabrynthWorldServer createWorldServer(PMDataTracker tracker,int dimID){
+    public static LabrynthWorldServer createWorldServer(PMDataTracker tracker,int dimID,LabrynthDetails details){
         World oworld = DimensionManager.getWorld(0);
         LabrynthWorldServer worldServer = new LabrynthWorldServer(MinecraftServer.getServer(),
                 oworld.getSaveHandler(),
@@ -17,7 +18,8 @@ public class LabrynthWorldServerFactory{
                 dimID,
                 new WorldSettings(oworld.getWorldInfo()),
                 MinecraftServer.getServer().theProfiler,
-                tracker
+                tracker,
+                details
         );
         // TODO: Do something to set the internal variables of LabrynthWorldServer
 
