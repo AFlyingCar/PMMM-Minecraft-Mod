1af9df0d0ed0d79a267863fab1cf58fa4dd5957e
Tyler Robbins <tyler@familyrobbins.com>
2016-08-22 17:24:33 -0700

    Attempted to fix various problems related to the creation and deletion of labrynths.
    Changed the NBTCompound structure that we save data as to more closely match PlayerDataTrackerManager and to fix issues where nothing would be saved. Added a field to map Labrynth entrances (by UUID) to their linked labrynth's details. Added getters for this new field. Deprecated a method. Moved some method calls around (too many to list and describe here). Added code to deleteData(LabrynthDetails) to make sure that a world is not loaded before unregistering it from DimensionManager. Rewrote storeLabrynthEntranceToRetrieveLater(...) so that we don't have to worry about the array becoming too full if left running for too long. Removed the call to checkAndUpdate from retrieveEntrance.

--- src/main/java/com/MadokaMagica/mod_madokaMagica/managers/LabrynthManager.java
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/managers/LabrynthManager.java
@@ -1,6 +1,9 @@
 package com.MadokaMagica.mod_madokaMagica.managers;
 
+import java.util.Map;
+import java.util.UUID;
 import java.util.List;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +29,7 @@ public class LabrynthManager{
     private int next;
 
     private List<LabrynthDetails> allDetails;
+    private Map<UUID,LabrynthDetails> entranceToDetailsMap;
 
     private boolean dirty;
     private boolean hasLoaded;
@@ -33,17 +37,36 @@ public class LabrynthManager{
 	private LabrynthManager(){
         entrances = new EntityPMWitchLabrynthEntrance[4096];
         allDetails = new ArrayList<LabrynthDetails>();
+        entranceToDetailsMap = new HashMap<UUID,LabrynthDetails>();
         dirty = true; // Save the first time it is loaded no matter what (This is for testing purposes)
         next = 0;
         hasLoaded = false;
 		// TODO: Finish this method
 	}
 
+    public List<LabrynthDetails> getDetailsList(){
+        return allDetails;
+    }
+
     public LabrynthDetails getDetailsByDimID(int id){
         for(LabrynthDetails details : allDetails){
             if(details.dimID == id){
                 return details;
             }
+        }
+        return null;
+    }
+
+    public LabrynthDetails getDetailsByEntrance(EntityPMWitchLabrynthEntrance epmwle){
+        if(entranceToDetailsMap.containsKey(epmwle.getPersistentID())){
+            return entranceToDetailsMap.get(epmwle.getPersistentID());
+        }
+        return null;
+    }
+
+    public LabrynthDetails getDetailsByEntranceUUID(UUID uuid){
+        if(entranceToDetailsMap.containsKey(uuid)){
+            return entranceToDetailsMap.get(uuid);
         }
         return null;
     }
@@ -66,6 +89,7 @@ public class LabrynthManager{
         next = 0;
         // Clear the other list
         allDetails.clear();
+        entranceToDetailsMap.clear();
         dirty = false;
     }
 
@@ -79,9 +103,21 @@ public class LabrynthManager{
      * Labrynth Details - related methods
      */
 
+    @Deprecated
     public void registerLabrynthDetails(LabrynthDetails details){
         allDetails.add(details);
-        this.markDirty(false);
+        this.markDirty(true);
+    }
+
+    public void registerLabrynthDetails(EntityPMWitchLabrynthEntrance epmwle, LabrynthDetails details){
+        entranceToDetailsMap.put(epmwle.getPersistentID(),details);
+        allDetails.add(details);
+        this.markDirty(true);
+    }
+
+    public void registerLabrynthDetails(UUID uuid, LabrynthDetails details){
+        entranceToDetailsMap.put(uuid,details);
+        this.markDirty(true);
     }
 
     public void markDirty(boolean dirty){
@@ -125,8 +161,38 @@ public class LabrynthManager{
             return;
         }
 
+        System.out.println("Building LabrynthDetails NBT structure.");
         int count = 0;
+        int i=-1;
+        for(Map.Entry<UUID,LabrynthDetails> entry : entranceToDetailsMap.entrySet()){
+            i++;
+            UUID entranceUUID = entry.getKey();
+            LabrynthDetails details = entry.getValue();
 
+            if(details.markForDestruction){
+                System.out.println("Labrynth #" + i + " has been found to be marked for destruction. Deleting.");
+                deleteLabrynth(details.dimID,true);
+                continue;
+            }
+
+            NBTTagCompound uld_nbt = new NBTTagCompound();
+
+            NBTTagCompound u_nbt = new NBTTagCompound();
+            u_nbt.setLong("MOST_SIG",entranceUUID.getMostSignificantBits());
+            u_nbt.setLong("LEAST_SIG",entranceUUID.getLeastSignificantBits());
+            uld_nbt.setTag("UUID",u_nbt);
+
+            NBTTagCompound ld_nbt = new NBTTagCompound();
+            ld_nbt.setInteger("dimID",details.dimID);
+            ld_nbt.setString("dimName",details.dimName);
+            uld_nbt.setTag("LAB_DETAILS",ld_nbt);
+
+            nbt.setTag(""+i,uld_nbt);
+            count++;
+        }
+        System.out.println("Built an NBT Structure of " + count + " Labrynths");
+
+        /*
         for(int i=0; i<allDetails.size(); i++){
             LabrynthDetails details = allDetails.get(i);
             // Do not save the labrynth if it has been marked to be destroyed
@@ -144,6 +210,7 @@ public class LabrynthManager{
             // Do this here because we don't want to count the details marked for destruction
             count++;
         }
+        */
         nbt.setInteger("MAX DETAILS",count);
 
         try{
@@ -219,35 +286,44 @@ public class LabrynthManager{
     }
 
     public void load(NBTTagCompound nbt){
+        NBTTagCompound ld_nbt = nbt.getCompoundTag("LAB_DETAILS");
+
         LabrynthDetails details = new LabrynthDetails();
-        details.dimID = nbt.getInteger("dimID");
-        details.dimName = nbt.getString("dimName");
+        details.dimID = ld_nbt.getInteger("dimID");
+        details.dimName = ld_nbt.getString("dimName");
 
         System.out.println("Attempting to load dimension #" + details.dimID + "("+details.dimName+")");
 
-        details.world = LabrynthWorldServer.loadFromNBT(nbt);
+        details.world = LabrynthWorldServer.loadFromNBT(ld_nbt);
 
         if(details.world != null){
             DimensionManager.registerDimension(details.dimID,MadokaMagicaConfig.labrynthProviderID);
             DimensionManager.setWorld(details.dimID,details.world);
         }
 
+        NBTTagCompound u_nbt = nbt.getCompoundTag("UUID");
+        UUID uuid = new UUID(u_nbt.getLong("MOST_SIG"),u_nbt.getLong("LEAST_SIG"));
+
+        this.entranceToDetailsMap.put(uuid,details);
+
         this.allDetails.add(details);
     }
-
-    /*
-    public void registerDetailsWithDimensionManager(LabrynthDetails details){
-        DimensionManager.registerDimension(details.dimID,MadokaMagicaConfig.labrynthProviderID);
-        DimensionManager.initDimension(details.dimID);
-    }
-    */
 
     /*
      * Methods related to Labrynth Entrances
      */
 
     public int storeLabrynthEntranceToRetrieveLater(EntityPMWitchLabrynthEntrance entity){
-        entrances[++next] = entity;
+        // Search for the next non-null slot
+        while(entrances[++next] != null)
+            if(next == entrances.length-1)
+                return -1;
+        entrances[next] = entity;
+        // If we've reached the end, head back to the beginning so we can restart
+        if(next == entrances.length-1){
+            next = 0;
+            return entrances.length-1;
+        }
         return next;
     }
 
@@ -255,10 +331,10 @@ public class LabrynthManager{
         EntityPMWitchLabrynthEntrance entrance = entrances[id];
         // Set it to null so we don't fill up entrances
         entrances[id] = null;
-        checkNextAndUpdate();
         return entrance;
     }
 
+    // NOTE: I don't think we need this method any more
     protected void checkNextAndUpdate(){
         // Make sure to reduce next whenever we can so we don't get an empty array where we are at the end and can't add any more entrances
         if(entrances[next] == null){
@@ -282,8 +358,20 @@ public class LabrynthManager{
     public boolean deleteLabrynth(int id,boolean deleteFolder){
         // NOTE: We must be very careful here, we don't want to delete the wrong dimension after all
         // Make sure we cast this so that it will throw an error if the world isn't a LabrynthWorldServer, which all Labrynths should have and which nothing else should have
+        /*
         LabrynthWorldServer world = (LabrynthWorldServer)DimensionManager.getWorld(id);
         LabrynthDetails details = world.details;
+        */
+        LabrynthDetails details = this.getDetailsByDimID(id);
+
+        // Make sure to fail if the id is not a valid dimension
+        if(details == null){
+            return false;
+        }
+
+
+        // Call this method out here because it unloads the dimension on purpose.
+        deleteData(details);
 
         // Make sure the world isn't in use before we attempt to delete it
         if(DimensionManager.getWorld(details.dimID) == null){
@@ -291,7 +379,7 @@ public class LabrynthManager{
                 deleteFiles(details);
             }
 
-            deleteData(details);
+            markDirty(true);
             return true;
         }
 
@@ -307,6 +395,25 @@ public class LabrynthManager{
 
     // Deletes the data from memory
     private void deleteData(LabrynthDetails details){
+        // TODO: Should we check if any players are in this dimension and teleport them out?
+        //  Or should we assume that the methods which mark this for deletion will do that for us? 
+
         allDetails.remove(details); // Not many places to delete it from
+
+        // Don't attempt to unregister it unless we know it is registered
+        if(DimensionManager.isDimensionRegistered(details.dimID)){
+            DimensionManager.setWorld(details.dimID,null); // Make sure the world is unloaded first
+            DimensionManager.unregisterDimension(details.dimID);
+        }
+
+        UUID mine=null;
+        for(Map.Entry<UUID,LabrynthDetails> entry : entranceToDetailsMap.entrySet()){
+            if(entry.getValue() == details){
+                mine = entry.getKey();
+                break;
+            }
+        }
+        if(mine != null)
+            entranceToDetailsMap.remove(mine);
     }
 }
