f19b878fa5408b1c5d1643fe3e7b41e621b8bb0e
Tyler Robbins <tyler@familyrobbins.com>
2017-07-11 18:47:25 -0700

    Added a new class to act as the generator for labrynths.

---
+++ src/main/java/com/MadokaMagica/mod_madokaMagica/world/LabrynthGenerator.java
@@ -0,0 +1,17 @@
+/**
+ * Tyler Robbins
+ * 12/12/16
+ * LabrynthGenerator.java
+ * 
+ */
+
+package com.MadokaMagica.mod_madokaMagica.world;
+
+import net.minecraft.world.gen.ChunkProviderGenerate;
+import net.minecraft.world.World;
+
+public class LabrynthGenerator extends ChunkProviderGenerate {
+    public LabrynthGenerator(World worldObj, long seed, boolean mapFeaturesEnabled) {
+        super(worldObj, seed, mapFeaturesEnabled);
+    }
+}
